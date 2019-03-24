8df3d865aa3944e32382e4f56f94e6dce734a053
Edward <eafanador691@unab.edu.co>
2019-03-24 00:56:19 -0500

    prototipo funcional
    base de datos funcional, login signup funcionales busqueda funcional, al igual que logout

--- Flask/hw/app.py
+++ Flask/hw/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, session, escape,redirect,url_for,flash,g
-
+from flask import Flask, render_template, request, session, escape,redirect,url_for,flash,g,send_from_directory
+from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -8,31 +8,69 @@ import os
 '''flash es para los mensajs y notificaciones despues de una accion'''
 dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
 
+UPLOAD_FOLDER = os.path.abspath("./uploads/")
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpge"])
+def allowed_file(filename):
+
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-class Base(db.Model):
-    __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
-    
+
+
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     
-"""class Palabras(Base):
+class Palabras(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Palabra = db.Column(db.String(100), unique=True, nullable=False)
-    images = db.relationship("Image", backref="owner", lazy="dynamic")
-    
-class Image(Base):
-    filename = db.Column(db.String(80), nullable=False)"""
+    filename = db.Column(db.String(80), nullable=False)
 
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        if not "file" in request.files:
+            return "No file part in the form."
+        f = request.files["file"]
+        if f.filename == "":
+            return  "no se ha seleccionado ningun archivo."
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            new_word = Palabras(Palabra=request.form["Palabra"],filename=filename)
+            db.session.add(new_word)
+            db.session.commit()
+            return redirect(url_for("get_file", filename=filename))
+        return "File not allowed."
+
+    return """<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Upload File</title>
+</head>
+<body>
+    <h1>Añadir al Diccionario</h1>
+    <form method="POST" enctype="multipart/form-data">
+        <p><input type="text" name="Palabra"></p>
+	    <input type="file" name="file">
+        <input type="submit" value="Añadir">
+    </form>
+</body>
+</html>"""
+
+@app.route("/uploads/<filename>")
+def get_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 @app.before_request
 def before_request():
@@ -118,8 +156,8 @@ def logout():
 
 app.secret_key = "12345"
 
+
 if __name__ == "__main__":
-    db.drop_all()
     db.create_all()
     app.run(debug=True)
 
